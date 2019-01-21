f63f27099aa4fc66d1d8e7201716ad0a50ce83ee
bartoszstepak <46901072+bartoszstepak@users.noreply.github.com>
2019-01-21 18:18:47 +0100

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+
+public class MainClass {
+    public static void main(String [] Args) {
+        EventQueue.invokeLater(() -> {
+            AppFrame app = new AppFrame();
+        });
+    }
+}
